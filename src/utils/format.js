6e532d5951a7d66aad46112065ae453d90c0848d
Your Name <donderileo@gmail.com>
2020-08-09 18:38:29 -0300

    Terminou o curso

---
+++ src/utils/format.js
@@ -0,0 +1,34 @@
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(subjectNumber){
+    return subjects[+subjectNumber-1]
+}
+
+
+
+module.exports = {
+    subjects,
+    weekdays,
+    getSubject
+}
